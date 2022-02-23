2e6bfc720c7b9ad36051119767b10ae79dc12e5d
zzq89757 <1604653835@qq.com>
2022-02-23 14:40:46 +0800

    ---

--- src/components/production/Product.vue
+++ src/components/production/Product.vue
@@ -1,45 +1,33 @@
 <!--  -->
 <template>
   <div class="pro">
-<<<<<<< HEAD
     <product-item v-for="item in data" :key="item.title">
       <img slot="picture" :src="item.src" />
       <h5 slot="title">{{ item.title }}</h5>
       <p slot="content">{{ item.content }}</p>
-=======
-    <product-item v-for="item in jsondata" :key="item.title" :goods="item">
->>>>>>> a5ba3f59c97bda5d2a18c1b358efa86b344bd9d2
     </product-item>
     <h2>{{ data }}</h2>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import ProductItem from "./ProductItem.vue";
 import { request } from "@/network/axios";
 
+import ProductItem from "./ProductItem.vue";
 export default {
   data() {
     return {
       data: "",
-=======
-// import jsondata from "@/assets/json/toolMsg.json";
-import ProductItem from './ProductItem.vue';
-export default {
-  data() {
-    return {
-      // jsondata,
->>>>>>> a5ba3f59c97bda5d2a18c1b358efa86b344bd9d2
     };
-  },props:{
-    data:{
-      type:Array,
-      requied:false,
-      default:[]
-    }
   },
-<<<<<<< HEAD
+  props: {
+    data: {
+      type: Array,
+      requied: false,
+      default: [],
+    },
+  },
   created() {
     request("/api/product").then(
       (res) => {
@@ -50,15 +38,13 @@ export default {
     );
   },
   components: { ProductItem },
-=======
-  props:{
-    jsondata:{
-      type:Array,
-      required:true,
-    }
+  props: {
+    jsondata: {
+      type: Array,
+      required: true,
+    },
   },
-  components: {ProductItem},
->>>>>>> a5ba3f59c97bda5d2a18c1b358efa86b344bd9d2
+  components: { ProductItem },
 
   computed: {},
 
